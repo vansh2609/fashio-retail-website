8d1485446836030e1d31d435530565cd3ae038f7
vansh2609 <152732618+vansh2609@users.noreply.github.com>
2023-12-03 00:35:14 +0530

    Add files via upload

---
+++ tailors-mind.js
@@ -0,0 +1,31 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "25%";
+  }
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+function category1() {
+  window.location.href="ethinicwear.html"
+}
+function category2() {
+  window.location.href="kidswear.html"
+}
+function category3() {
+  window.location.href="westernwear.html"
+}
+function category4() {
+  window.location.href="workwear.html"
+}
+function category5() {
+  window.location.href="sizewear.html"
+}
+function category6() {
+  window.location.href="jwellery.html"
+}
+function category7() {
+  window.location.href="activewear.html"
+}
+function category8() {
+  window.location.href="casualwear.html"
+}
